f278504af3aa83be0295b5c2fcfefe0b31beb676
Skai <tyronekisienya01@gmail.com>
2025-10-04 13:40:45 +0300

    2. Batch processing Large Data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import mysql.connector
+from mysql.connector import Error
+from seed import DB_CONFIG, DB_NAME
+
+def stream_users_in_batches(batch_size):
+    try:
+        conn = mysql.connector.connect(
+            host = DB_CONFIG['host'],
+            port = DB_CONFIG['port'],
+            user = DB_CONFIG['user'],
+            password = DB_CONFIG['password'],
+            database = DB_NAME
+        )
+
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM user_data')
+
+        batch = []
+        for row in cursor:
+            batch.append(row)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        cursor.close()
+        conn.close()
+    
+    except Error as e:
+        print('Error fetching batch', e)
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        filtered = [user for user in batch if float(user['age']) > 25]
+        print(f'processed batch with {len(filtered)} users over 25')
+        yield filtered
