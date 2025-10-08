36e728c9de49a45e3a81d40d630f8e2f800ddc6b
Skai <tyronekisienya01@gmail.com>
2025-10-08 12:45:02 +0300

    context class manager for auto connect and close database

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+    def __enter__(self):
+        self.conn = sqlite3.connect('users.db')
+        print(f'Connected')
+        return self.conn
+    def __exit__(self, type, value, traceback):
+        if self.conn:
+            self.conn.close()
+            print(f'Closed Successfully')
+        return False
+    
+with DatabaseConnection('users.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute('SELECT * from users')
+    results = cursor.fetchall()
+    print(f'Query results:', results)
