f8cf8ecffb311b9af6311d753ad51a44bf3d63c6
Skai <tyronekisienya01@gmail.com>
2025-10-16 16:45:21 +0300

    test memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -45,13 +45,15 @@ class TestGetJson(unittest.TestCase):
 class TestMemoize(unittest.TestCase):
     '''decorator memoize test'''
     def test_memoize(self):
-        
+        '''test function intialised'''
         class TestClass:
+            '''test class for the function'''
             def a_method(self):
                 return 42
             
             @memoize
             def a_property(self):
+                '''memoize code'''
                 return self.a_method()
             
         with patch.object(TestClass, "a_method", return_value = 42) as mock_method:
