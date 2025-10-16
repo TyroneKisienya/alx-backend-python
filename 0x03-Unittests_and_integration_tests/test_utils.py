4152b60d429c16f864f03cac2a25343414e52e8b
Skai <tyronekisienya01@gmail.com>
2025-10-16 15:31:28 +0300

    test 2 to test error key raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,5 +15,14 @@ class TestAccessNestedMap(unittest.TestCase):
         '''test that access_nested_map returns correct stuff'''
         self.assertEqual(access_nested_map (nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, error_key):
+        '''test that error is handled correctly'''
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), f"'{error_key}'")
 if __name__ == "__main__":
     unittest.main()
