ff16dee305eecb5cb370b587a6b3f8a72caaa14b
Skai <tyronekisienya01@gmail.com>
2025-10-17 08:09:22 +0300

    test for the has_license staticmethod

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -72,6 +72,15 @@ class TestGithubOrgClient(unittest.TestCase):
             ("https://api.github.com/orgs/google/repos")
             Mock_public_repos_url.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        '''Test the has_license staticmethod'''
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
