1ab9cfcad71cbfffaf6017cadd29be4be854cf04
Skai <tyronekisienya01@gmail.com>
2025-10-24 19:30:39 +0300

    views and urls in chat dir

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,9 @@
-from .views import ConversationsViewSet, MessageViewSet
+from .views import ConversationViewSet, MessageViewSet
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
 router = DefaultRouter()
-router.register(r'conversations', ConversationsViewSet, basename='conversation')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'conversations/(?P<conversation_id>[^/.]+)/messages', MessageViewSet,basename='message')
 url_pattern = [
     path('api/')
