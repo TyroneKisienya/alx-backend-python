575af62d6b9a4435c74c03328a2196c2d50c5337
Skai <tyronekisienya01@gmail.com>
2025-10-24 11:43:50 +0300

    rest_framework session auths

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -121,3 +121,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#REST AUTHENTICATION CLASSES
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
