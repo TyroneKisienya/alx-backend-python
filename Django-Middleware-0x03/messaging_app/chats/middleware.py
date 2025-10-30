fef7a72dd2b55bc5b758e92155c3570f61547626
Skai <tyronekisienya01@gmail.com>
2025-10-30 12:50:41 +0300

    message time limitation

--- Django-Middleware-0x03/messaging_app/chats/middleware.py
+++ Django-Middleware-0x03/messaging_app/chats/middleware.py
@@ -1,5 +1,6 @@
 import logging
-from datetime import datetime
+from datetime import datetime, time
+from django.http import HttpResponseForbidden
 
 def logger():
     logger = logging.getLogger(__name__)
@@ -19,11 +20,23 @@ class RequestLoggingMiddleware:
 
     def __call__(self, request):
         user = request.user
-        log_entry = (
-            'f"{datetime.now()} - User: {user} - Path: {request.path}“'
-        )
+        log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}“
         self.logger.info(log_entry)
 
         response = self.get_response(request)
 
+        return response
+    
+class RequestAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.forbid_start = time(21, 0)
+        self.forbid_end  = time(6,0)
+
+    def __call__(self,request):
+        now = datetime.now().time()
+
+        if (self.forbid_start <= now) or (now < self.forbid_end):
+            return HttpResponseForbidden('Restricted Access')
+        response = self.get_response(request)
         return response
