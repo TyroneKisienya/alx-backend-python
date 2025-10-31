34d7f7eac1d14b211f358009b86ed2246952f217
Skai <tyronekisienya01@gmail.com>
2025-10-31 12:25:00 +0300

    created the first notification model and inheritance is on AbstractUser

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -3,12 +3,15 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
-class User(AbstractUser):
-    user_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True)
-    
 class Message(models.Model):
-    sender = models.ForeignKey(default=uuid.uuid4)
-    receiver = models.ForeignKey(default=uuid.uuid4)
-    content = models.TextField(max_length=200)
+    sender = models.ForeignKey(AbstractUser,default=uuid.uuid4, related_name='sent_messages')
+    receiver = models.ForeignKey(AbstractUser, default=uuid.uuid4, related_name='received_messages')
+    content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add = True)
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(AbstractUser, on_delete=models.CASCADE, related_name='notifications')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, null=True)
+    content = models.CharField(max_length=128)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
