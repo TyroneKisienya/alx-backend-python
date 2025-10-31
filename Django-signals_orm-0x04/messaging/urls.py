0aee35d8235651053b9fb1513f6827fa028d0ee6
Skai <tyronekisienya01@gmail.com>
2025-10-31 13:55:13 +0300

    created a delete class to allow deletion

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import MessageViewset
 from django.urls import path, include
 
+
 router = DefaultRouter
 router.register(r'message', MessageViewset)
 
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import action
 from rest_framework import viewsets, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import AbstractUser, Message, MessageHistory, Notification
 from .serializers import MessageSerializer, MessageHistoryserializer, NotificationSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class MessageViewset(viewsets.ModelViewSet):
@@ -17,3 +19,13 @@ class MessageViewset(viewsets.ModelViewSet):
 
         serializer = MessageSerializer(history_records, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class Accountdelete(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def delete(self, request ,*args, **kwargs):
+        user = request.user
+        user.delete()
+        return Response(
+            {'details: delete_user'}, status= status.HTTP_204_NO_CONTENT
+        )
