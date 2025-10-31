8e1f0047d58dc067e8f6f95ae314a9862cdbf75c
Skai <tyronekisienya01@gmail.com>
2025-10-31 13:43:41 +0300

    created views for the app and a new path

---
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import MessageViewset
+from django.urls import path, include
+
+router = DefaultRouter
+router.register(r'message', MessageViewset)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import AbstractUser, Message, MessageHistory, Notification
+from .serializers import MessageSerializer, MessageHistoryserializer, NotificationSerializer
 
 # Create your views here.
+class MessageViewset(viewsets.ModelViewSet):
+    queryset = Message.objects.all().order_by('-timestamp')
+    serializer_class = MessageSerializer
+
+    @action(detail=True, methods=['GET'])
+    def history(self, request, pk=None):
+        message =get_object_or_404(Message, pk=pk)
+        history_records = message.history.all()
+
+        serializer = MessageSerializer(history_records, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
