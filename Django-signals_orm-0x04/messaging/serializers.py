36400811a73693fde1a31b4092e4b49c854b76c5
Skai <tyronekisienya01@gmail.com>
2025-10-31 14:43:25 +0300

    updated the messageSerializer

--- Django-signals_orm-0x04/messaging/serializers.py
+++ Django-signals_orm-0x04/messaging/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from .models import AbstractUser, Message, Notification, MessageHistory
 
+class RecursiveMessageSerializer(serializers.ModelSerializer):
+    def to(self, value):
+        serializer = MessageSerializer(value, context= self.context)
+        return serializer.data
 class MessageHistoryserializer(serializers.ModelSerializer):
     class Meta:
         models = MessageHistory
@@ -12,6 +16,14 @@ class NotificationSerializer(serializers.ModelSerializer):
         fields  =['message', 'content']
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = AbstractUser(read_only = True)
+    receiver = AbstractUser(read_only = True)
+    history = MessageHistory(many= True, read_only= True)
+    replies = RecursiveMessageSerializer(many = True, read_only = True)
     class Meta:
         models = Message
-        fields = ['sender', 'content', 'timestamp']
+        fields = ['sender', 'content', 'timestamp', 'receiver', 'parent_message', 'edited']
+        read_only_fields = ['content', 'timestamp', 'parent_message', 'receiver']
+
+        def create(self, validated_data):
+            return Message.objects.create(**validated_data)
