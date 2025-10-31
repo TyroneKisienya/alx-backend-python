9d1b345be5899affe1ec38c65efdd92e65ec30a9
Skai <tyronekisienya01@gmail.com>
2025-10-31 12:15:45 +0300

    created the first message model

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    user_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True)
+    
+class Message(models.Model):
+    sender = models.ForeignKey(default=uuid.uuid4)
+    receiver = models.ForeignKey(default=uuid.uuid4)
+    content = models.TextField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add = True)
